818dc905b88498fc1989fe2d97f3f53a797c30c2
TaslimaNasrin <taslimanasrin.318@gmail.com>
2022-06-16 12:08:26 +0530

    Create CreateArtifactProperties.py

---
+++ CreateArtifactProperties.py
@@ -0,0 +1,19 @@
+from xml.etree import ElementTree
+from xml.dom import minidom
+import os
+import re
+import sys
+
+POM_FILE="pom.xml"
+
+namespaces = {'xmlns' : 'http://maven.apache.org/POM/4.0.0'}
+tree = ElementTree.parse(POM_FILE)
+root = tree.getroot()
+rel_ver = root.find("./xmlns:version", namespaces=namespaces)
+rel_gid = root.find("./xmlns:groupId", namespaces=namespaces)
+rel_aid = root.find("./xmlns:artifactId", namespaces=namespaces)
+#print(rel_ver.text)
+#print(rel_gid.text)
+file = open("artifact.properties", "w")
+file.write("GAVC=" +rel_gid.text + ":" + rel_aid.text + ":" +rel_ver.text)
+file.close
